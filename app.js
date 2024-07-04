d04cf2417173970b048d8664df2a65ee69533e32
Kailane da Silva Ribeiro <dasilvaribeirokailane@gmail.com>
2024-07-04 10:04:14 -0300

    Update app.js

--- app.js
+++ app.js
@@ -18,33 +18,33 @@ numero1 = parseFloat(numero1)
 numero2 = parseFloat(numero2)
 
     //fazer a logica das operações no switch
-    if (!isNaN(num1) && !isNaN(num2)) {
-    switch (operação) {
-        case '1':
-        resultado = numero1 + numero2
-        console.log(` ${numero1} + ${numero2} = `)
-        break;
-        case '2':
-        resultado = numero1 - numero2
-        console.log(` ${numero1} - ${numero2} = `)
-        break;
-        case '3':
-        resultado = numero1 * numero2
-        console.log(` ${numero1} X ${numero2} = `)
-        break;
-        case '4':
-        if (numero1 ===0 || numero2 ===0) {
-        console.log(" Erro, divisão por 0! ")        
-        }else{
-        resultado = numero1/numero2
-        console.log(` ${numero1} / ${numero2} = `)
-        }
-        break;
-        case '5':
-        resultado = numero1 * numero2/100
-        console.log( `${numero1} % ${numero2} = `)
-        break;
-        default:
+    if (!isNaN(numero1) && !isNaN(numero2)) {
+        switch (operação) {
+            case '1':
+            resultado = numero1 + numero2
+            console.log(` ${numero1} + ${numero2} = `)
+            break;
+            case '2':
+            resultado = numero1 - numero2
+            console.log(` ${numero1} - ${numero2} = `)
+            break;
+            case '3':
+            resultado = numero1 * numero2
+            console.log(` ${numero1} X ${numero2} = `)
+            break;
+            case '4':
+            if (numero1 ===0 || numero2 ===0) {
+            console.log(" Erro, divisão por 0! ")        
+            }else{
+            resultado = numero1/numero2
+            console.log(` ${numero1} / ${numero2} = `)
+            }
+            break;
+            case '5':
+            resultado = numero1 * numero2/100
+            console.log( `${numero1} % ${numero2} = `)
+            break;
+            default:
 console.log(" OPERAÇÃO INVALIDA. ")
         break;
 }
