1db7fcdf385ca701e296ae696d247a5460ac0fdd
Kailane da Silva Ribeiro <dasilvaribeirokailane@gmail.com>
2024-07-04 11:47:01 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var numero2 = prompt('Digite o segundo numero: ')
 console.log(`
     -*- Calculadora -*-
     1 - Soma
-    2 - Subtracao
-    3 - Multiplicacao
+    2 - Subtração
+    3 - Multiplicação
     4 - Divisão
     5 - Porcentagem
 `)
@@ -16,37 +16,36 @@ var resultado
 
 numero1 = parseFloat(numero1)
 numero2 = parseFloat(numero2)
-
-    //fazer a logica das operações no switch
-    if (!isNaN(numero1) && !isNaN(numero2)) {
-        switch (operação) {
-            case '1':
+//fazer a logica das operações no switch
+if (!isNaN(numero1) && !isNaN(numero2)) {
+    switch (operação) {
+        case '1':
             resultado = numero1 + numero2
             console.log(` ${numero1} + ${numero2} = `)
             break;
-            case '2':
+        case '2':
             resultado = numero1 - numero2
             console.log(` ${numero1} - ${numero2} = `)
             break;
-            case '3':
+        case '3':
             resultado = numero1 * numero2
             console.log(` ${numero1} X ${numero2} = `)
             break;
-            case '4':
+        case '4':
             if (numero1 ===0 || numero2 ===0) {
-            console.log(" Erro, divisão por 0! ")        
+                console.log(" Erro, divisão por 0! ")        
             }else{
-            resultado = numero1/numero2
-            console.log(` ${numero1} / ${numero2} = `)
+                resultado = numero1/numero2
+                console.log(` ${numero1} / ${numero2} = `)
             }
             break;
-            case '5':
+        case '5':
             resultado = numero1 * numero2/100
             console.log( `${numero1} % ${numero2} = `)
             break;
-            default:
-console.log(" OPERAÇÃO INVALIDA. ")
-        break;
+        default:
+            console.log(" OPERAÇÃO INVALIDA. ")
+            break;
 }
     console.log(resultado)
 } else {
